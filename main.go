5219b227a1c32a9d63142f4d132771e229de0a07
Federico Ragona <federico.ragona@gmail.com>
2023-04-13 18:20:13 +0200

    Create sqlite3 db file if it doesn't exist

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"os"
 
 	"github.com/fedragon/ark/migrations"
 
@@ -16,21 +17,36 @@ import (
 const dbPath = "ark.db"
 
 func main() {
-	sqldb, err := sql.Open(sqliteshim.ShimName, fmt.Sprintf("file:%s?cache=shared&mode=rw", dbPath))
+	db, err := connect(dbPath)
 	if err != nil {
 		panic(err)
 	}
-	defer sqldb.Close()
-
-	db := bun.NewDB(sqldb, sqlitedialect.New())
+	defer db.Close()
 
 	migrator := migrate.NewMigrator(db, migrations.Migrations)
-
 	if err := migrator.Init(context.Background()); err != nil {
 		panic(err)
 	}
-
 	if _, err := migrator.Migrate(context.Background()); err != nil {
 		panic(err)
 	}
+}
+
+func connect(dbPath string) (*bun.DB, error) {
+	if _, err := os.Stat(dbPath); err != nil {
+		if os.IsNotExist(err) {
+			if _, err := os.Create(dbPath); err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+
+	sqldb, err := sql.Open(sqliteshim.ShimName, fmt.Sprintf("file:%s?cache=shared&mode=rw", dbPath))
+	if err != nil {
+		return nil, err
+	}
+
+	return bun.NewDB(sqldb, sqlitedialect.New()), nil
 }
