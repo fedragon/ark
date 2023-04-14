864fe377d869228e0031925bb96caed1c21f6b88
Federico Ragona <federico.ragona@gmail.com>
2023-04-14 08:10:30 +0200

    Add CLI flags

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -21,9 +22,12 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/urfave/cli/v2 v2.25.1 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -11,38 +12,83 @@ import (
 
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/afero"
+	"github.com/urfave/cli/v2"
 )
 
-const dbPath = "ark.db"
+const (
+	dbPathFlag    = "db"
+	sourceFlag    = "source"
+	archiveFlag   = "archive"
+	fileTypesFlag = "file-types"
+)
 
 func main() {
-	now := time.Now()
-	var appFs = afero.NewOsFs()
 
-	repo, err := db.NewSqlite3Repository(appFs, dbPath)
-	if err != nil {
-		panic(err)
+	app := &cli.App{
+		Usage:           "a CLI to manage an archive of media files",
+		UsageText:       "ark [global options]",
+		Version:         "0.1.0",
+		HideHelpCommand: true,
+		Flags: []cli.Flag{
+			&cli.PathFlag{
+				Name:  dbPathFlag,
+				Value: "./ark.db",
+				Usage: "Path to the SQLite3 database file. Will be created if it doesn't exist.",
+			},
+			&cli.PathFlag{
+				Name:     sourceFlag,
+				Required: true,
+				Usage:    "Absolute path of the directory containing the files to be imported.",
+			},
+			&cli.PathFlag{
+				Name:     archiveFlag,
+				Required: true,
+				Usage:    "Absolute path of the archive directory.",
+			},
+			&cli.StringSliceFlag{
+				Name:  fileTypesFlag,
+				Value: cli.NewStringSlice(".cr2", ".jpg", ".jpeg", ".mov", ".mp4", ".orf"),
+				Usage: "File types to archive.",
+			},
+		},
 	}
-	defer repo.Close()
 
-	imp := importer.Imp{
-		Repo:      repo,
-		Fs:        appFs,
-		FileTypes: []string{".cr2", ".jpg", ".jpeg"},
+	app.Action = func(c *cli.Context) error {
+		dbPath, err := homedir.Expand(c.String(dbPathFlag))
+		if err != nil {
+			return err
+		}
+		source, err := homedir.Expand(c.String(sourceFlag))
+		if err != nil {
+			return err
+		}
+		archive, err := homedir.Expand(c.String(archiveFlag))
+		if err != nil {
+			return err
+		}
+		fileTypes := c.StringSlice(fileTypesFlag)
+
+		now := time.Now()
+		defer func() { fmt.Println("Elapsed time:", time.Since(now)) }()
+
+		var appFs = afero.NewOsFs()
+
+		repo, err := db.NewSqlite3Repository(appFs, dbPath)
+		if err != nil {
+			return err
+		}
+		defer repo.Close()
+
+		imp := importer.Imp{
+			Repo:      repo,
+			Fs:        appFs,
+			FileTypes: fileTypes,
+		}
+
+		return imp.Import(context.Background(), source, archive)
 	}
 
-	source, err := homedir.Expand(os.Args[1])
-	if err != nil {
-		panic(err)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err.Error())
 	}
-	target, err := homedir.Expand(os.Args[2])
-	if err != nil {
-		panic(err)
-	}
-
-	if err := imp.Import(context.Background(), source, target); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Elapsed time:", time.Since(now))
 }
